e23a0a978c2b9c786e559ac2d729e242ae2c2ab9
Murali.Kodavati <Murali.Kodavati@davintaad.com>
2017-08-17 11:41:42 +0530

    Murali changes

---
+++ src/main/java/org/spring/model/security/AuthorityName.java
@@ -0,0 +1,5 @@
+package org.spring.model.security;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/org/spring/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.spring.security.repository;
+
+import org.spring.model.security.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by stephan on 20.03.16.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
